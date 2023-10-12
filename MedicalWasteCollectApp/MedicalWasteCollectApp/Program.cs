35baf02e99b56932123a4de291fc180b057c56d3
Marek D <marek.wroc@gmail.com>
2023-10-12 11:38:41 +0200

    Truck almost ready

--- MedicalWasteCollectApp/MedicalWasteCollectApp/Program.cs
+++ MedicalWasteCollectApp/MedicalWasteCollectApp/Program.cs
@@ -4,96 +4,29 @@ Truck truck3 = new Truck("SK269HT");
 Truck truck4 = new Truck("SD53722");
 
 truck1.AddWaste(15);
+var loadsSum = truck1.LoadsSum;
 
 class Truck
 {
-    private string regNumber;
+    private List<int> loads = new List<int>();
 
     public Truck(string regNumber)
     {
         this.RegNumber = regNumber;
-        this.load = 0;
     }
 
     public string RegNumber { get; private set; }
 
-    public viod AddWaste(int load)
-    { 
-        this.load += load;
+    public int LoadsSum
+    {
+        get
+        {
+            return this.loads.Sum();
+        }
     }
-}
 
-List<int> loads = new List<int>();
-loads.Add(76);
-loads.Add(37);
-loads.Add(13);
-loads.Add(54);
-loads.Add(32);
-
-for (var i = 0; i < loads.Count; i++)
-{
-    Console.WriteLine(loads[i]);
-}
-
-var loadsSum = 0;
-
-foreach (var load in loads)
-{
-    Console.WriteLine(load);
-
-    loadsSum = loadsSum + load;
-}
-
-Console.WriteLine(loadsSum);
-
-string loadsAsString = loadsSum.ToString();
-char[] letters = loadsAsString.ToArray();
-
-int[] counter = new int[10];
-foreach (char letter in letters)
-{
-    if (letter == '0')
+    public void AddWaste(int load)
     {
-        counter[0]++;
-    } 
-    else if (letter == '1')
-    {
-        counter[1]++;
+        this.loads.Add(load);
     }
-    else if (letter == '2')
-    {
-        counter[2]++;
-    }
-    else if (letter == '3')
-    {
-        counter[3]++;
-    }
-    else if (letter == '4')
-    {
-        counter[4]++;
-    }
-    else if (letter == '5')
-    {
-        counter[5]++;
-    }
-    else if (letter == '6')
-    {
-        counter[6]++;
-    }
-    else if (letter == '7')
-    {
-        counter[7]++;
-    }
-    else if (letter == '8')
-    {
-        counter[8]++;
-    }
-    else if (letter == '9')
-    {
-        counter[9]++;
-    }
-}
-for(var i=0; i<counter.Length; i++)
-{
-    Console.WriteLine(i+" => " + counter[i]);
 }
